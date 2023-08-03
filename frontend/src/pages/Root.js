8ba424229388aeb37fd43b6a55fe863c46e896ed
bishal <bishals10@gmail.com>
2023-08-03 13:43:28 +0300

    add automatic logout after 1 hour

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 export default function RootLayout() {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    if (!token === "EXPIRED") {
+      submit.action(null, { action: "/logout", method: "post" });
+    }
+
+    const tokenDuration = getTokenDuration();
+    setTimeout(() => {
+      submit.action(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
